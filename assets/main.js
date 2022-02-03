3361c732fc6d30dd9fa2b92f7323d3d94449278c
dierkholm <dierkholm@gmail.com>
2022-02-03 17:30:54 +0500

    erase loop from js

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,5 @@
 console.log('hello world')
 const swiper = new Swiper('.featured__swiper-products', {
-    loop: true,
-
     // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
