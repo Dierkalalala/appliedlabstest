ddba97ca505d0b7c53341bfff77e3804b84d8023
dierkholm <dierkholm@gmail.com>
2022-02-03 17:18:56 +0500

    add swiper

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,9 @@
 console.log('hello world')
+const swiper = new Swiper('.featured__swiper-products', {
+    loop: true,
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
