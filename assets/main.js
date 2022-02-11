c7447e60ce327878e70b77f1ac12b33c7ca03c3d
dierkholm <dierkholm@gmail.com>
2022-02-11 15:06:14 +0500

    product page template markup

--- assets/main.js
+++ assets/main.js
@@ -21,18 +21,6 @@ const swiper = new Swiper('.featured__swiper-products', {
         }
     }
 })
-class API {
-    constructor() {
-        this.baseUrl = '';
-    }
-    static get(url) {
-        return axios.get(url)
-    }
-
-    static post (url, data) {
-        return axios.post(url, data)
-    }
-}
 
 (function ProductWorkSpace() {
     let productOptionChanger = document.querySelectorAll('.product__option-wrapper label')
