777674e56e619fc578842fa194fb66aa17771db1
dierkholm <dierkholm@gmail.com>
2022-02-11 14:01:07 +0500

    cart workarounds

--- assets/cart.js
+++ assets/cart.js
@@ -1,36 +1,48 @@
 class Cart {
     constructor(props) {
         this.items = [];
-        this.cartItemMarkUp = `
-            <tr class="cart__row">
-              <td class="cart__meta small--text-left">
-                <div class="cart__product-information">
-                  <div class="cart__image-wrapper">
-                    <img class="cart__image" alt="" src="">
-                  </div>
-                  <div>
-                    <div class="list-view-item__title">
-                      <a href="" class="cart__product-title">
-                        
-                      </a>
+        this.cartItemMarkUp = Handlebars.compile(`
+            {{#each items as | item |}}
+                <tr class="cart__row">
+                  <td class="cart__meta small--text-left">
+                    <div class="cart__product-information">
+                      <div class="cart__image-wrapper">
+                        <img class="cart__image" alt="{{ item.featured_image.alt }}" src="{{ item.featured_image.url }}">
+                      </div>
+                      <div>
+                        <div class="list-view-item__title">
+                          <a href="" class="cart__product-title">
+                            {{ item.product_title }}
+                          </a>
+                        </div>
+    
+                        <ul class="product-details">
+                           {{#each item.variant_options}}
+                                <li class="product-details__item product-details__item--property">{{.}}</li>
+                           {{/each}}
+                        </ul>
+                        <p class="cart__remove">
+                          <a href="/cart/change?line={{@index}}&amp;quantity=0" class="text-link text-link--accent">Remove</a>
+                        </p>
+                      </div>
                     </div>
-
-                    <ul class="product-details">
-                      <li class="product-details__item product-details__item--variant-option"></li>
-                      <li class="product-details__item product-details__item--property"></li>
-                    </ul>
-
-                    <p class="cart__remove">
-                      <a href="/cart/change?line={{ forloop.index }}&amp;quantity=0" class="text-link text-link--accent">Remove</a>
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td class="cart__price text-right">
-                
-              </td>
-            </tr>
-        `
+                  </td>
+                  <td class="cart__price text-right">
+                    {{ item.final_price }}
+                  </td>
+                  <td class="cart__quantity-td text-right">
+                    <div class="cart__qty">
+                        <input class="cart__qty-input" type="number" value="{{ item.quantity }}" min="0" pattern="[0-9]*">
+                    </div>
+                  </td>
+                  <td class="cart__final-price text-right">                 
+                    <div>
+                      <span>{{ item.final_line_price }}</span>
+                    </div>
+                  </td>
+                </tr>
+            {{ /each }}
+        `);
         this.getCartItems();
     }
 
@@ -38,7 +50,9 @@ class Cart {
     getCartItems() {
         return API.get('/cart.js')
             .then(res => {
-                console.log(res)
+                console.log(
+                    this.cartItemMarkUp({items: res.data.items})
+                )
             })
             .catch(err => {
                 console.log(err);
